cd43de8346c51964d2ee1e5301961e9e581d4dd3
demas <demasmamas@gmail.com>
2022-11-23 22:40:40 +0300

    add constructor page test add gh-pages

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,17 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { defineConfig } from 'cypress';
+import { defineConfig } from "cypress";
 
 export default defineConfig({
   component: {
     devServer: {
-      framework: 'create-react-app',
-      bundler: 'webpack',
+      framework: "create-react-app",
+      bundler: "webpack",
+    },
+  },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
     },
   },
 });
